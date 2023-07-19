faeb2ce73720828cae6be689c7c3fc264aee6ae5
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-19 14:44:51 +0200

    fixed vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
-const path = require('path')
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+const path = require('path');
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -13,7 +17,8 @@ export default defineConfig({
     resolve: {
         alias: {
             '~resources' : '/resources/',
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
+
+            '~bootstrap' : path.resolve(__dirname, 'node_modules/bootstrap'),
         }
     }
 });
