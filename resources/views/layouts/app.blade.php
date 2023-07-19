99b38477b923d5dd71a93df3cdbb62a3d5a825ef
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-19 15:06:32 +0200

    included header & footer partials in app, added some yield to insert custom content when you use the template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Laravel App')</title>
+
+    @vite('resources/js/app.js')
+    @yield('custom-stylesheets')
+    @yield('custom-scripts-head')
 </head>
+
 <body>
+    @include('partials.header')
+
     <main>
         @yield('main-section')
     </main>
 
-    @vite('resources/js/app.js')
+    @include('partials.footer')
+    
+    @yield('custom-scripts-tail')
 </body>
 </html>
